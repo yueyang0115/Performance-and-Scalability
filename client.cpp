26a948146239601cdf84b39dfec8c3647778f44b
kw300 <kw300@duke.edu>
2020-04-01 20:36:03 -0400

    client modified try

--- client.cpp
+++ client.cpp
@@ -17,28 +17,40 @@ void * sendRequest(void * arg) {
   Thread_arg * thr_arg = (Thread_arg *)arg;
   int bucket = thr_arg->bucketID;
   int delay = thr_arg->delay;
-  int threadID = thr_arg->threadID;
-  int socket_fd = thr_arg->client_fd;
+  //int threadID = thr_arg->threadID;
+  //int socket_fd = thr_arg->client_fd;
 
-  //send request
-  int random = rand() % bucket;
-  string l1 = to_string(delay) + "," + to_string(random) + "\n";
-  const char * request = l1.c_str();
-  cout << "thread " << threadID << " sends Request: " << request;
-  send(socket_fd, request, strlen(request), 0);
+  while (1) {
+    //setup client
+    const char * hostname = "0.0.0.0";
+    const char * port = "12345";
+    int socket_fd = build_client(hostname, port);
+    if (socket_fd == -1) {
+      std::cout << "Error in build client!\n";
+    }
 
-  //receive response
-  char response[20];
-  memset(response, 0, sizeof(request));
-  int len = recv(socket_fd, response, sizeof(response), 0);
-  if (len < 0) {
-    return NULL;
+    //cout << "create a thread, ID is : " << i << endl;
+
+    //send request
+    int random = rand() % bucket;
+    string l1 = to_string(delay) + "," + to_string(random) + "\n";
+    const char * request = l1.c_str();
+    cout << " sends Request: " << request;
+    send(socket_fd, request, strlen(request), 0);
+
+    //receive response
+    char response[20];
+    memset(response, 0, sizeof(request));
+    int len = recv(socket_fd, response, sizeof(response), 0);
+    if (len < 0) {
+      return NULL;
+    }
+
+    string l2 = response;
+    double value = stoi(l2);
+    cout << "new value in bucket[" << random << "]: " << value << endl;
+    close(socket_fd);
   }
-
-  string l2 = response;
-  double value = stoi(l2);
-  cout << "new value in bucket[" << random << "]: " << value << endl;
-  close(socket_fd);
 
   return NULL;
 }
@@ -53,31 +65,23 @@ int main(int argc, char * argv[]) {
   int bucket = atoi(argv[2]);
 
   pthread_t * threads;
-  int numThreads = 2000;
+  int numThreads = 200;
   threads = (pthread_t *)malloc(numThreads * sizeof(pthread_t));
 
   srand((unsigned int)time(NULL));
   for (int i = 0; i < numThreads; i++) {
-    //setup client
-    const char * hostname = "0.0.0.0";
-    const char * port = "12345";
-    int socket_fd = build_client(hostname, port);
-    if (socket_fd == -1) {
-      std::cout << "Error in build client!\n";
-    }
-
-    cout << "create a thread, ID is : " << i << endl;
     Thread_arg * thr_arg = new Thread_arg();
     thr_arg->delay = delay;
     thr_arg->bucketID = bucket;
-    thr_arg->threadID = i;
-    thr_arg->client_fd = socket_fd;
+    //thr_arg->threadID = i;
+    //thr_arg->client_fd = socket_fd;
+
     pthread_create(&threads[i], NULL, sendRequest, thr_arg);
-    pthread_detach(threads[i]);
+    //pthread_detach(threads[i]);
   }
   for (int i = 0; i < numThreads; i++) {
     pthread_join(threads[i], NULL);
   }
-  
+
   return 0;
 }
